11b2ff4ae3dc86584792c00cf893151b58b7eb52
dylanp-01 <dperry2407@gmail.com>
2025-09-26 18:04:02 -0400

    Added main.py
    Implements basic layer system using 2D array

---
+++ main.py
@@ -0,0 +1,19 @@
+y: int = 30
+x: int = 40
+
+def newlayer(y: int, x: int, fill: str = ""):
+    layer = []
+    for i in range(y):
+        row = []
+        for j in range(x):
+            row.append(fill)
+        layer.append(row)
+    return layer
+
+def printlayer(layer, deliminator: str = "\n"):
+    string = ""
+    for y in range(len(layer)):
+        for x in range(len(layer[y])):
+            string += layer[y][x]
+        string += deliminator
+    print(layer)
